c28e965c38fdf6db8a7dfbf4d6495373c214f929
Kyle McParland <kylemcparland@gmail.com>
2024-07-10 21:09:58 -0400

    refactored play.js and moved function to client.js

---
+++ client.js
@@ -0,0 +1,30 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+
+  conn.setEncoding("utf-8");
+
+  conn.on("data", (data) => {
+    console.log("Message from server:", data);
+  });
+
+  conn.on("connect", () => {
+    console.log("You have connected successfully!")
+  });
+
+  conn.on("close", () => {
+    console.log("You have been disconnected!");
+  });
+
+  conn.on("error", (error) => {
+    console.log("Error ocurred: ", error);
+  });
+
+  return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,35 +1,4 @@
-const net = require("net");
-
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
-  });
-
-  conn.setEncoding("utf-8");
-
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  conn.on("connect", () => {
-    console.log("You have connected successfully!")
-  });
-
-  conn.on("close", () => {
-    console.log("You have been disconnected!");
-  });
-
-  conn.on("end", () => {
-    console.log("The game has been closed!");
-  });
-
-  conn.on("error", (error) => {
-    console.log(`Error ocurred: ${error}`);
-  });
-
-  return conn;
-};
+const connect = require("./client");
 
 console.log("Connecting ...");
 connect();
