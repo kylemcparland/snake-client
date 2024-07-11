9fc6b3232586a080b79f0660134b0fa270769406
Kyle McParland <kylemcparland@gmail.com>
2024-07-10 22:27:03 -0400

    added movement functionality to input.js

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;  // create variable to hold the stdin object so we don't have to type process.stdin multiple times
   stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
   stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
@@ -11,6 +14,22 @@ const handleUserInput = function (key) {
   if (key === "\u0003") {
     console.log("Disconnecting...");
     process.exit();
+  }
+  //up
+  if (key === "\u0077") {
+    connection.write("Move: up");
+  }
+  //down
+  if (key === "\u0073") {
+    connection.write("Move: down");
+  }
+  //left
+  if (key === "\u0061") {
+    connection.write("Move: left");
+  }
+  //right
+  if (key === "\u0064") {
+    connection.write("Move: right");
   }
 };
 
