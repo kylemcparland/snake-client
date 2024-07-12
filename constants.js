bd7ce140a248d55e260dc51ce0704490f3c1d8b4
Kyle McParland <kylemcparland@gmail.com>
2024-07-12 10:59:36 -0400

    added constants.js to handle inputs in an object + IP and PORT variables

---
+++ constants.js
@@ -0,0 +1,19 @@
+const IP = "localhost";
+const PORT = 50541;
+const input = {
+  w: "Move: up",
+  s: "Move: down",
+  a: "Move: left",
+  d: "Move: right",
+  //
+  h: "Say: HELLO",
+  l: "Say: LOL",
+  g: "Say: GG",
+  o: "Say: OMG",
+};
+
+module.exports = {
+  IP,
+  PORT,
+  input,
+};
--- input.js
+++ input.js
@@ -1,3 +1,4 @@
+const input = require("./constants").input;
 let connection;
 
 const setupInput = function(conn) {
@@ -15,44 +16,8 @@ const handleUserInput = function(key) {
     console.log("Disconnecting...");
     process.exit();
   }
-  //up
-  if (key === "\u0077") {
-    connection.write("Move: up");
-  }
-  //down
-  if (key === "\u0073") {
-    connection.write("Move: down");
-  }
-  //left
-  if (key === "\u0061") {
-    connection.write("Move: left");
-  }
-  //right
-  if (key === "\u0064") {
-    connection.write("Move: right");
-  }
-  //messages:
-  if (key === "\u0068") {
-    connection.write("Say: HELLO");
-  }
-  if (key === "\u006C") {
-    connection.write("Say: LOL");
-  }
-  if (key === "\u0067") {
-    connection.write("Say: GG");
-  }
-  if (key === "\u006F") {
-    connection.write("Say: OMG");
-  }
+  connection.write(input[key]);
 };
-
-/*
-One small suggestion is to consider using a mapping object for the movement
-commands instead of multiple if statements. This could make your code more concise
-and easier to maintain. For example, you could create an object where the keys are
-the WASD keys and the values are the corresponding directions. Then, you could look
-up the direction based on the key pressed and send the command to the server
-*/
 
 module.exports = {
   setupInput
